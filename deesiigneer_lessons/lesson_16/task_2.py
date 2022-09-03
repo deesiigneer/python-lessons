3e917c53dc4bdba239875eeb51fd2c8a3db41ac4
deesiigneer <xdeesiigneerx@gmail.com>
2022-09-03 17:38:38 +0300

    changes fix #9

--- deesiigneer_lessons/lesson_16/task_2.py
+++ deesiigneer_lessons/lesson_16/task_2.py
@@ -3,6 +3,6 @@ while True:
         x, y = map(int, input('Введите координат для первой точки через пробел: ').split())
         x1, y1 = map(int, input('Введите значение координат для второй точки через пробел: ').split())
         from math import sqrt
-        print(round(sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1)), 3))
+        print(round(sqrt((x-x1)**2+(y-y1)**2), 3))
     except ValueError:
         print('Ошибочка получается...')
--- deesiigneer_lessons/lesson_16/task_3.py
+++ deesiigneer_lessons/lesson_16/task_3.py
@@ -1,6 +1,6 @@
 while True:
     try:
         chicken, cow, pig = map(int, input('Введите кол-во кур, коров и свиней через пробел: ').split())
-        print(f'Кол-во ног всех животных: {(chicken*2)+(cow*4)+(pig*4)}')
+        print(f'Кол-во ног всех животных: {(chicken*2)+(cow+pig)*4}')
     except ValueError:
         print('Ошибочка получается...')
