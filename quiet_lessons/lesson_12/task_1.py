e62ca80ae7efe55fb89e62665532ed55bbc0ab35
Quiet <nastbacherova@mail.ru>
2022-09-09 00:24:16 +0300

    close #11

--- quiet_lessons/lesson_12/task_1.py
+++ quiet_lessons/lesson_12/task_1.py
@@ -1,7 +1,7 @@
 while True:
     try:
         count_stars = int(input('Введите кол-во строк: '))
-        for _ in range(count_stars + 1):
-            print('*' * _)
+        for _ in range(count_stars):
+            print('*' * (_ + 1))
     except ValueError:
         print('Некорректный тип ввода данных!')
