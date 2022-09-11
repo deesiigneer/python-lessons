1fdef8d79a4749516778c729913741a79c2a6cf3
Quiet <nastbacherova@mail.ru>
2022-09-11 21:50:42 +0300

    перенесла преобразование в лист #13

--- quiet_lessons/lesson_32/task_1.py
+++ quiet_lessons/lesson_32/task_1.py
@@ -1,9 +1,9 @@
 while True:
     try:
-        first_list = map(int, input('Введите перечень цифр через запятую: ').split(','))
-        first_list = [i for i in list(first_list) if i % 2 != 0]
-        second_list = map(int, input('Введите второй перечень цифр через запятую: ').split(','))
-        second_list = [i for i in list(second_list) if i % 2 == 0]
+        first_list = list(map(int, input('Введите перечень цифр через запятую: ').split(',')))
+        first_list = [i for i in first_list if i % 2 != 0]
+        second_list = list(map(int, input('Введите второй перечень цифр через запятую: ').split(',')))
+        second_list = [i for i in second_list if i % 2 == 0]
         joined_list = first_list + second_list
         print(f'Результат: {joined_list}')
     except ValueError:
